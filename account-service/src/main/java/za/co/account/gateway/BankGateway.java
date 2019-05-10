6639fa9aa517e0d0f04279028f317564036f22eb
Mayank Tantuway <mayankt@discovery.co.za>
2019-05-10 14:01:15 +0200

    Enabled Hystrix for account service
    Change-Id: I0d07a6266352480c5dbe4bc0ffa9678082f3fa77

--- account-service/src/main/java/za/co/account/gateway/BankGateway.java
+++ account-service/src/main/java/za/co/account/gateway/BankGateway.java
@@ -1,14 +1,17 @@
 package za.co.account.gateway;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "bank-service", fallback = BankGatewayFallbackService.class)
 public interface BankGateway {
 
-    @RequestMapping("/api/bank/{id}")
+    @RequestMapping(value = "/api/bank/{id}" , method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "getAccountDetailsByAccountId")
     String getAccountDetailsByAccountId(@PathVariable("id") Long accountId);
 
 }
